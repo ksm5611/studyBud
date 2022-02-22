50978dd981d99e767b1849086f39662abb0d3882
sumin <kim.sumin.wat2014@gmail.com>
2022-02-22 13:45:08 -0800

    created message model

--- base/views.py
+++ base/views.py
@@ -1,23 +1,17 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-  {'id':1, 'name':'Lets learn python!'},
-  {'id':2, 'name':'Design with me'},
-  {'id':3, 'name':'Frontend developers'}
-]
 
 def home(req):
+  rooms = Room.objects.all()
   context = {'rooms': rooms}
   return render(req, 'base/home.html', context)
 
 def room(req, pk):
-    room = None
-    for i in rooms:
-      if i['id'] == int(pk):
-        room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(req, 'base/room.html', context)
