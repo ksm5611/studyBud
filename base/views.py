fd2546be74065ec74ff5651e3614d0ef19f5359e
sumin <kim.sumin.wat2014@gmail.com>
2022-02-22 17:26:16 -0800

    created delete and edit button

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
@@ -5,22 +6,41 @@ from .forms import RoomForm
 # Create your views here.
 
 
-def home(req):
+def home(request):
   rooms = Room.objects.all()
   context = {'rooms': rooms}
-  return render(req, 'base/home.html', context)
+  return render(request, 'base/home.html', context)
 
-def room(req, pk):
+def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
-    return render(req, 'base/room.html', context)
+    return render(request, 'base/room.html', context)
 
-def createRoom(req):
+def createRoom(request):
   form = RoomForm()
-  if req.method == 'POST':
-    form = RoomForm(req.POST)
+  if request.method == 'POST':
+    form = RoomForm(request.POST)
     if form.is_valid():
       form.save()
       return redirect('home')
   context = {'form': form}
-  return render(req, 'base/room_form.html', context)
+  return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+  room = Room.objects.get(id=pk)
+  form = RoomForm(instance=room)
+
+  if request.method == 'POST':
+    form = RoomForm(request.POST, instance=room)
+    if form.is_valid():
+      form.save()
+      return redirect('home')  
+  context = {'form': form}
+  return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+  room = Room.objects.get(id=pk)
+  if request.method == 'POST':
+    room.delete()
+    return redirect('home')
+  return render(request, 'base/delete.html', {'obj': room})
