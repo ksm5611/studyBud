24b0bec132d7c5f1f8cd3404dab7b9d29d1a2275
sumin <kim.sumin.wat2014@gmail.com>
2022-02-22 16:34:58 -0800

    created room form

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from multiprocessing import context
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -15,3 +17,13 @@ def room(req, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(req, 'base/room.html', context)
+
+def createRoom(req):
+  form = RoomForm()
+  if req.method == 'POST':
+    form = RoomForm(req.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  context = {'form': form}
+  return render(req, 'base/room_form.html', context)
