730100db1271fb4c30818a1822e2d99f8116ead1
sumin <kim.sumin.wat2014@gmail.com>
2022-02-23 16:02:50 -0800

    filtered topic and implemented url query

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 
 def home(request):
-  rooms = Room.objects.all()
-  context = {'rooms': rooms}
+  q = request.GET.get('q') if request.GET.get('q') != None else ''
+  rooms = Room.objects.filter(topic__name__contains=q)
+  topics = Topic.objects.all()
+  context = {'rooms': rooms, 'topics': topics}
   return render(request, 'base/home.html', context)
 
 def room(request, pk):
