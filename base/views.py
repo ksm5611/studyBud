6492742d356295793fb4429cef4a013b8e1458d4
sumin <kim.sumin.wat2014@gmail.com>
2022-02-24 17:57:16 -0800

    created regi page and implemented regi form

--- base/views.py
+++ base/views.py
@@ -6,17 +6,20 @@ from django.db.models import Q
 from .models import Room, Topic
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .forms import RoomForm
 
 # Create your views here.
 
 def loginPage(request):
+  page = 'login'
+
   # to prevent user re-login
   if request.user.is_authenticated:
     return redirect('home')
 
   if request.method == 'POST':
-    username = request.POST.get('username')
+    username = request.POST.get('username').lower()
     password = request.POST.get('password')
 
     try:
@@ -32,12 +35,28 @@ def loginPage(request):
     else:
       messages.error(request, 'Username or password does not exist')
     
-  context = {}
+  context = {'page': page}
   return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+def registerPage(request):
+  form = UserCreationForm()
+
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'An error occurred during registration')
+
+  return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
@@ -49,9 +68,9 @@ def home(request):
   return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = Room.objects.get(id=pk)
-    context = {'room': room}
-    return render(request, 'base/room.html', context)
+  room = Room.objects.get(id=pk)
+  context = {'room': room}
+  return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
